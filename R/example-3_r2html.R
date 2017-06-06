2d26acc00568269d45bce0a38053dd0049663a33
annakrystalli <annakrystalli@googlemail.com>
2017-06-06 17:12:36 +0100

    commit batch

---
+++ R/example-3_r2html.R
@@ -0,0 +1,66 @@
+
+#' # All-in-one `.R` example
+#' 
+#' In this example I keep all the code and annotation in this `.Rmd` document. To extract the analysis workflow code into an `.R` script, I'm going to use function `knitr::purl()`.
+#' 
+#' Some of the code chunks are useful in the context of this document but I don't really want to export them to the `.R` as part of the workflow. I indicate these chunks by using chunk option `purl = FALSE`.
+#' 
+#' 
+#' ***
+#' 
+#' ## Analysis description:
+#' 
+#' For the purposes of this talk I've sourced this example from: https://datascienceplus.com/k-means-clustering-in-r/ and using the `iris` dataset to demo the workflow. 
+#' 
+#' ####  Background: What is K Means Clustering?
+#' 
+#' **K Means Clustering** is an unsupervised learning algorithm that tries to cluster data based on their similarity. Unsupervised learning means that there is no outcome to be predicted, and the algorithm just tries to find patterns in the data. In k means clustering, we have to specify the number of clusters we want the data to be grouped into. The algorithm randomly assigns each observation to a cluster, and finds the centroid of each cluster. Then, the algorithm iterates through two steps:
+#' Reassign data points to the cluster whose centroid is closest.
+#' Calculate new centroid of each cluster.
+#' 
+#' These two steps are repeated till the within cluster variation cannot be reduced any further. The within cluster variation is calculated as the sum of the euclidean distance between the data points and their respective cluster centroids.
+#' 
+#' ## Analysis setup
+#+ ## ----analysis-setup------------------------------------------------------
+library(datasets)
+library(ggplot2)
+library(knitr)
+
+#' 
+#' 
+#' ## Explore the data
+#' 
+#' 
+#' 
+#' 
+#+ ## ----plot-data-----------------------------------------------------------
+ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
+
+
+#' 
+#' 
+#' 
+#' ## Fit kmeans model
+#+ ## ----model---------------------------------------------------------------
+set.seed(20)
+irisCluster <- kmeans(iris[, 3:4], 3, nstart = 20)
+
+#' 
+#' ### explore results
+#' 
+#' 
+#' 
+#+ ## ----results-table-------------------------------------------------------
+table(irisCluster$cluster, iris$Species)
+
+#' 
+#' ### Plot results
+#' 
+#+ ## ----results-plot--------------------------------------------------------
+irisCluster$cluster <- as.factor(irisCluster$cluster)
+ggplot(iris, aes(Petal.Length, Petal.Width, 
+                 color = irisCluster$cluster,
+                 shape = Species)) + 
+    geom_point()
+
+#' 
--- R/render.R
+++ R/render.R
@@ -1,13 +1,23 @@
-depends <- c("markdown", "knitr")
+depends <- c("rmarkdown", "knitr")
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(depends, character.only = T)
 
+## ---- purl-rmd ----
 file <- "example-rmd1"
-for(docu in 1:3){
-    knitr::purl(input = paste0("Rmd/", file, .Rmd"), 
+for(docu in 0:2){
+    knitr::purl(input = paste0("Rmd/", file, ".Rmd"), 
                 output = paste0("R/", file, "_", docu,".R"), 
                 documentation = docu)
 }
 
 
+
+## ---- render-rmd ----
+file <- "presentation"
+render(input = paste0("Rmd/", file, ".Rmd"),
+       html_document(theme = "journal"),
+       output_file = paste0("../handout/", file, ".html"))
+
+## ---- spin-r ----
+spin("R/example-3_r2html.R")
